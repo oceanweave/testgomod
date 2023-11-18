7b386fd86af76db022b94917c6d89f2979a607f7
Fengyang <495160589@qq.com>
2023-11-18 15:26:17 +0800

    2-4. 建议包package名和所在目录名保持一致，这样import引入时不会有歧义，同时更改包名后可能会报错，重新打开项目载入就可正常

--- main.go
+++ main.go
@@ -13,10 +13,13 @@ import (
 	// 由于此处末尾是 demo1，而该路径下的包是 demox
 	// 容易造成如下混乱：1. 不好理解，为什么上面是 demo1，而 demox 执行正常 2. 不便于排错，若 demox 的函数出现问题，不熟悉代码的人不清楚该包所在位置
 	// 所以此处，为该 import 进行重命名，标识，以便容易理解
-	demox "github.com/oceanweave/testgomod/pkg/demo1"
+
+	// 2-4 最终推荐，还是保持包名和目录名一致，这样没有歧义，便于理解和排错
+	"github.com/oceanweave/testgomod/pkg/demo1"
 )
 
 func main() {
 	// 上面 import 引用正确的路径后，就可以正常调用执行
-	demox.Hello1()
+	// 注意更改包名后，可能此处调用会报错，因此项目还没有刷新过来，此时关闭并重新打开该项目，就可以变为正常了
+	demo1.Hello1()
 }
--- pkg/demo1/hello1.go
+++ pkg/demo1/hello1.go
@@ -1,5 +1,5 @@
-// 更改包名
-package demox
+// 更改包名 保持和目录名一致  这样 import 时候不会产生歧义
+package demo1
 
 import "fmt"
 
