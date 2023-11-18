7fb178a6930485b0360a53187f0644963840e48b
Fengyang <495160589@qq.com>
2023-11-18 15:18:57 +0800

    2-3. hello1.go 的包名和所在目录名不一致的情况下，利用 import 包时的重命名机制，进行重命名以便容易理解和排错以及问题定位

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 	// github.com/oceanweave/testgomod 是项目名，go.mod 的名称，可以理解为当前项目的根目录
 	// 因此 github.com/oceanweave/testgomod/pkg/demo1 就可以指向 demox 包
 	// 之后下面就可以调用 demox 包
-	"github.com/oceanweave/testgomod/pkg/demo1"
+
+	// 2-3 利用包的重命名
+	// 由于此处末尾是 demo1，而该路径下的包是 demox
+	// 容易造成如下混乱：1. 不好理解，为什么上面是 demo1，而 demox 执行正常 2. 不便于排错，若 demox 的函数出现问题，不熟悉代码的人不清楚该包所在位置
+	// 所以此处，为该 import 进行重命名，标识，以便容易理解
+	demox "github.com/oceanweave/testgomod/pkg/demo1"
 )
 
 func main() {
