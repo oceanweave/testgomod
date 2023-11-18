4b6b89698af71b6baf4f2165425d63f2811c5174
Fengyang <495160589@qq.com>
2023-11-18 15:14:39 +0800

    2-2. hello1.go 的包名和所在目录名不一致的情况下，其实是需要 import 指定包的路径，而不是包的名称，此时可以正常执行

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	// 引用新改的包名 demox
-	"github.com/oceanweave/testgomod/pkg/demox"
+	// 2-1 引用新改的包名 demox，发现 go run main.go 无法运行
+	//"github.com/oceanweave/testgomod/pkg/demox"
+
+	// 2-2 引用包的路径，发现可以运行
+	// github.com/oceanweave/testgomod 是项目名，go.mod 的名称，可以理解为当前项目的根目录
+	// 因此 github.com/oceanweave/testgomod/pkg/demo1 就可以指向 demox 包
+	// 之后下面就可以调用 demox 包
+	"github.com/oceanweave/testgomod/pkg/demo1"
 )
 
-// 引用新改的包名，发现 go run main.go 无法运行
 func main() {
+	// 上面 import 引用正确的路径后，就可以正常调用执行
 	demox.Hello1()
 }
